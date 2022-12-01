ce95ceac34f2111823537cfb7f26bced6396cf02
danielsafari143 <danielsafari143@gmail.com>
2022-12-01 10:16:24 +0100

    Update contents

--- cours.js
+++ cours.js
@@ -4,28 +4,28 @@ export const courses = {
     title: 'Html',
     link: 'https://www.w3schools.com/html/default.asp',
     image: 'https://cdn-icons-png.flaticon.com/512/186/186320.png',
-    texte: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    texte: 'This course aims to give you the necessary knowledge in html. ',
   },
   css: {
     author: 'safari hamuli',
     title: 'Css',
     link: 'https://www.w3schools.com/css/default.asp',
     image: 'https://cdn-icons-png.flaticon.com/512/186/186319.png',
-    texte: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    texte: 'This course aims to give you the necessary knowledge in css. ',
   },
   javascript: {
     author: 'Queen zuma',
     title: 'JavaScript',
     link: 'https://www.w3schools.com/js/default.asp',
     image: 'https://cdn-icons-png.flaticon.com/128/136/136530.png',
-    texte: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    texte: 'This course aims to give you the necessary knowledge in js. ',
   },
   python: {
     author: 'Rodrigue Kamanda',
     title: 'Python',
     link: 'https://www.w3schools.com/python/default.asp',
     image: 'https://cdn-icons-png.flaticon.com/512/5968/5968350.png',
-    texte: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    texte: 'This course aims to give you the necessary knowledge in pyton . ',
   },
 };
 export default courses;
